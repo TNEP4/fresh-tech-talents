3e0c9bd79360d7897f80db67496dc4c67de6cbd8
Nic Touron <nicolas.touron@kedgebs.com>
2022-02-22 10:24:21 +1100

    Head copy changes

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
     <div className='bg-gradient-to-br from-zinc-900 via-zinc-900 to-zinc-800'>
       <Head>
         <title>Fresh-tech-talents | Launch your career in tech</title>
-        <meta name="description" content="Find rising tech talents open to work. Launch your career in tech, showcase your best projects." />
+        <meta name="description" content="Find rising tech talents open to work, browse their best projects and profile for free." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
