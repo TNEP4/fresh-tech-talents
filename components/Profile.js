4931c924f840cbca33bdb24bdab0ab695cedb139
Nic Touron <nicolas.touron@kedgebs.com>
2022-02-26 21:40:16 +1100

    Added profile page for signed in user

--- components/Profile.js
+++ components/Profile.js
@@ -3,7 +3,7 @@ import { RiLinkedinBoxFill, RiTwitterFill, RiGithubFill } from "react-icons/ri";
 import { collection, getDocs, onSnapshot, query, orderBy, where, doc, getDoc, deleteDoc, updateDoc, setDoc, docSnap } from '@firebase/firestore';
 
 import { db, auth } from '../utils/firebase';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../utils/context';
 
 
@@ -11,25 +11,45 @@ export default function Profile() {
 
     const { user, username } = useContext(UserContext);
     console.log(user);
+    
+    // Set page data
+    const [displayName, setDisplayName] = useState("Loading...");
+    const [bio, setBio] = useState('Loading...');
+    const [interests, setInterests] = useState(['Loading...']);
+    const [languages, setLanguages] = useState(['Loading...']);
+    const [prefers, setPrefers] = useState(['Loading...']);
+    const [stack, setStack] = useState(['Loading...']);
+    const [portfolioUrl, setPortfolioUrl] = useState('portfolio-url.com');
+    const [location, setLocation] = useState('Loading...');
+    const [socialGithub, setSocialGithub] = useState('');
+    const [socialLinkedin, setSocialLinkedin] = useState('');
+    const [socialTwitter, setSocialTwitter] = useState('');
+    const [openToWork, setOpenToWork] = useState(false);
 
     useEffect(() => {
         if (user) {
         const uid = auth.currentUser.uid;
         console.log(uid);
         const docRef = doc(db, "users", uid);
-        }});
+        const docSnap = getDoc(docRef)
+        .then((doc) => {
 
-    // Get one document
-    // const page = [];
-    
-
-
-    // Set page data
-    // const [pageTitle, setPageTitle] = useState('Loading...');
-    // const [pageUrl, setPageUrl] = useState('Loading...');
-    // const [pageDescription, setPageDescription] = useState('Loading...');
-    // const [pageSummary, setPageSummary] = useState('Loading...');
-    // const [keywords, setKeywords] = useState('Loading...');
+            docSnap = doc.data();
+            console.log('docSnap', docSnap);
+            setDisplayName(docSnap.displayName);
+            setBio(docSnap.bio);
+            setInterests(docSnap.interests);
+            setLanguages(docSnap.languages);
+            setPrefers(docSnap.prefers);
+            setStack(docSnap.stack);
+            setPortfolioUrl(docSnap.portfolioUrl);
+            setLocation(docSnap.location);
+            setSocialGithub(docSnap.socialGithub);
+            setSocialLinkedin(docSnap.socialLinkedin);
+            setSocialTwitter(docSnap.socialTwitter);
+            setOpenToWork(docSnap.openToWork);
+        })
+        }}, [user]);
 
     // useEffect(() => {
     //     const docSnap = getDoc(docRef)
@@ -61,7 +81,7 @@ export default function Profile() {
                         <div className="flex flex-col w-full h-full justify-between">
                             <div className="flex flex-row justify-between">
                                 <div className="flex flex-row space-x-4">
-                                    { user ? <p className="text-white font-bold">{user.displayName}</p> : <p className="text-white font-bold">Loading</p>}
+                                    <p className="text-white font-bold">{displayName}</p>
                                     <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium text-green-400 bg-gradient-to-br from-zinc-800 to-zinc-700">
                                         <svg className="-ml-0.5 mr-1 h-3 w-3 text-green-400 animate-pulse" fill="currentColor" viewBox="0 0 8 8">
                                             <circle cx={4} cy={4} r={3} />
@@ -71,28 +91,28 @@ export default function Profile() {
                                 </div>
                                 
                                 <div className="space-x-2 flex flex-row items-center">
-                                    <a href="#" target="_blank" rel ="noopener noreferrer">
+                                    <a href={socialGithub} target="_blank" rel ="noopener noreferrer">
                                         <RiGithubFill className="h-6 w-6 cursor-pointer text-zinc-200 hover:text-zinc-50" />
                                     </a>
-                                    <a href="#" target="_blank" rel ="noopener noreferrer">
+                                    <a href={socialLinkedin} target="_blank" rel ="noopener noreferrer">
                                         <RiLinkedinBoxFill className="h-6 w-6 cursor-pointer text-zinc-200 hover:text-zinc-50" />
                                     </a>
-                                    <a href="#" target="_blank" rel ="noopener noreferrer">
+                                    <a href={socialTwitter} target="_blank" rel ="noopener noreferrer">
                                         <RiTwitterFill className="h-6 w-6 cursor-pointer text-zinc-200 hover:text-zinc-50" />
                                     </a>
                                 </div>
                             </div>
-                            <p className="text-green-400 underline underline-offset-1 font-medium cursor-alias">portfolio-url.com</p>
-                            <p>Long profile description, bio of about 1 sentence to explain why this talent is amazing and how unique.</p>
+                            <a href={portfolioUrl} target="_blank" rel ="noopener noreferrer" className="text-green-400 underline underline-offset-1 font-medium">{portfolioUrl}</a>
+                            <p>{bio}</p>
                         </div>
                     </div>
                 </div>
                 <div>
                     <p className="font-bold">About</p>
-                    <p><span className="text-zinc-400">Based in: </span> Los Angeles, California</p>
-                    <p><span className="text-zinc-400">Interested in: </span>Biotech, Fintech, Aviation</p>
-                    <p><span className="text-zinc-400">Prefers: </span>Remote work, Distributed teams</p>
-                    <p><span className="text-zinc-400">Speaks: </span>English, French</p>
+                    <p><span className="text-zinc-400">Based in: </span> {location}</p>
+                    <p><span className="text-zinc-400">Interested in: </span>{interests}</p>
+                    <p><span className="text-zinc-400">Prefers: </span>{prefers}</p>
+                    <p><span className="text-zinc-400">Speaks: </span>{languages}</p>
                 </div>
                 <div className="space-y-1">
                     <p className="font-bold">Stack</p>
